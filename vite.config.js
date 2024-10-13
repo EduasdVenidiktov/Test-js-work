28d54f573cec455cd31b39097d658a63d37a3a58
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-13 21:01:43 +0300

    with alert

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   base: "./", // Базовый путь для ресурсов
+//   resolve: {
+//     alias: {
+//       toastr: "toastr/build/toastr.min.js",
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Базовый путь для ресурсов
+  base: "./", // Путь к твоему репозиторию
   resolve: {
     alias: {
       toastr: "toastr/build/toastr.min.js",
